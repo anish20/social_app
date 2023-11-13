21311e27cc3e2c3acce187cf7858853ff1f595f3
anish20 <anish@jabitsoft.com>
2023-11-13 23:04:17 +0530

    first commit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import  './Login.scss'
+
+function Login() {
+  return (
+    <div className="login">
+        <div className="cards">
+            <div className="left">
+                
+            </div>
+            <div className="right"></div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Login
